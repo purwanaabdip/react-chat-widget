e4b17415824388c77676e5951d139197c00142ee
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2018-02-24 13:43:25 +0700

    successfully mock `document.getElementById()`

--- mocks/setup.js
+++ mocks/setup.js
@@ -1,26 +1,14 @@
 const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-16');
-const { JSDOM } = require('jsdom');
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-const win = new JSDOM('<!doctype html><html><body></body></html>');
-const doc = win.window.document;
-
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
-  Object.defineProperties(target, props);
-}
-
-global.window = win;
-global.document = doc;
-global.navigator = {
-  userAgent: 'node.js',
-};
-copyProps(win, global);
+// mock document.getElementById('id').scrollHeight
+Object.defineProperty(document, 'getElementById', {
+  value () {
+    return {
+      'scrollHeight': 80
+    }
+  }
+});
